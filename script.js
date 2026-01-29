2aacf3adc790fbfdbb13ecdcea579b0ead07a37e
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-29 23:26:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,30 +2,21 @@ const slider = document.querySelector('.items');
 
 let isDown = false;
 let startX;
-let scrollLeft;
+let startScrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+  startX = e.pageX;
+  startScrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-
-slider.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', (e) => {
   if (!isDown) return;
-  e.preventDefault();
 
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // scroll speed
-  slider.scrollLeft = scrollLeft - walk;
+  const walk = startX - e.pageX;
+  slider.scrollLeft = startScrollLeft + walk;
+});
+
+document.addEventListener('mouseup', () => {
+  isDown = false;
 });
